934fe33ab3b7be20265eb520ab31a134387cde46
BrianMere <brianmere@cox.net>
2023-11-29 16:32:31 -0800

    Goertzel is the right idea but the plots need to be better interpreted for fixes

---
+++ dft.py
@@ -0,0 +1,79 @@
+import cmath
+import math
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+def dft(xn : list[float], N : int) -> list[complex]:
+    """Do the dft with some input signal `xn` over a range of samples `N`
+    
+    Evaluates the dft at all 0 <= k < N
+    """
+    ks = [complex(0,0)]*N
+    j = complex(0,1)
+
+    for k in range(0, N):
+        sum : complex = 0
+        for i in range(0,N):
+            sum += xn[i]*cmath.exp(-1 * j * 2 * math.pi * k * i / N)
+        ks[k] = sum / complex(N,0)
+    return ks
+
+def dft_mag(xn : list[float], N : int) -> list[float]:
+    c_l = dft(xn, N)
+    for i in range(0, N):
+        c_l[i] = cmath.polar(c_l[i])[0]
+    return c_l
+
+def get_f0_mag(kn : list[float], fs : float) -> float:
+    """Get some weighted "peak" frequency from the formula given."""
+    sumtop = 0
+    sumbot = 0
+    T = max(kn)
+    for k in range(0, len(kn)//2):
+        if(kn[k] > T/2):
+            mag = kn[k]
+            f_k = fs * k / len(kn)
+            sumtop += f_k * mag
+            sumbot += mag
+    if sumbot != 0:
+        return sumtop / sumbot
+    return 0
+
+def plot_dft(xn : list[float], N : int, fs : float, fname : str | None):
+
+    fig, ax = plt.subplots()
+    kn = [0.0] * int(fs/2)
+    Xk = dft_mag(xn, N)
+    for i in range(0, N):
+        kn[i] = i / N * fs
+
+    ax.plot(np.array(kn[0:len(kn)//2]), np.array(Xk[0:len(Xk)//2]))
+
+    ax.set(xlabel='Frequency (Hz)', ylabel='Magnitude', title='DFT Plot')
+    ax.grid()
+
+    fig.savefig('image_file.png')
+
+    
+    
+    with open(f'{fname}.txt', 'w') as f:
+        f.write(f"Data for {fname}: ...\n")
+        # freq of the peak
+        f.write(f"fp: {kn[Xk.index(max(Xk))]}\n")
+        # freq of the peak using weighted average
+        freqAvg = get_f0_mag(Xk, fs)
+        f.write(f"f0: {freqAvg}\n")
+        # measured air gap in inches
+        airGap = 343*39.3701*0.5/freqAvg	
+        f.write(f"ag: {airGap} (in)\n")
+        f.close()
+    
+    if fname == None:
+        plt.show()
+    else:
+        plt.savefig(f"{fname}.png")
+
+
+    
+
--- goertzel.py
+++ goertzel.py
@@ -1,31 +1,69 @@
 
-from bandpass import DifferenceEquation
+from bandpass import DifferenceEquation, BandpassFilter
+from dft import plot_dft
+
 import math
 import cmath
 
-def goertzel(xn : list[int], k : int) -> complex:
-    """A Gortzel-Style Filter. 
+class BPGoertzel:
+    """A list of passed frequencies that want to be detected are passed, given 
+    some sampling frequency. This class gives information 
     
-    All this is is a filter that does two things in parallel:
-
-    1) Apply an IIR filter to get s[n]
-    2) While s[n] is being calculated, get y[n] from s[n]
-
-    Args:
-        xn: Signal data dump of some length N
-        k: Get the k-th DFT constant from doing xn. Thus, the analyzed frequency is wo = 2pi*k/N
+    This is pretty much a bandpass filter on the frequency in question, as 
+    well as doing the actual goertzel calculation from that filter's 
+    information. 
     """
-    N = len(xn)
-    j = complex(0.0, 1.0)
-    wo = 2*math.pi*k / N
-    sn_eq = DifferenceEquation([-2*math.cos(wo), 1], [], 1.0)
-    sn : list[int] = sn_eq.yn(xn)
-    return cmath.exp(j * 2 * cmath.pi * k / N)*sn[N-1] - sn[N-2]
+
+    def __init__(self, f_m : float, f_s : float):
+        self.f_m = f_m
+        self.f_s = f_s
+        self.bp_filt = BandpassFilter(0.9, 2.1, f_m, f_s)
+
+    def get_mag(self, xn : list) -> float:
+        """Get the magnitude of our class's f_m value. 
+        
+        Args:
+            xn: Input data to calculate all at once. 
+        """
+        k = round(self.f_m / self.f_s) * len(xn)
+        input_sig = self.bp_filt.de.yn(xn)
+        return abs(self.goertzel(input_sig, k))
+
+    def goertzel(self, xn : list[int], k : int) -> complex:
+        """A Gortzel-Style Filter. 
+        
+        All this is is a filter that does two things in parallel:
+
+        1) Apply an IIR filter to get s[n]
+        2) While s[n] is being calculated, get y[n] from s[n]
+
+        Args:
+            xn: Signal data dump of some length N
+            k: Get the k-th DFT constant from doing xn. Thus, the analyzed frequency is wo = 2pi*k/N
+        """
+        N = len(xn)
+        j = complex(0.0, 1.0)
+        wo = 2*math.pi*k / N
+        sn_eq = DifferenceEquation([-2*math.cos(wo), 1], [], 1.0)
+        sn : list[int] = sn_eq.yn(xn)
+        return cmath.exp(j * 2 * cmath.pi * k / N)*sn[N-1] - sn[N-2]
+
 
 
 if __name__ == "__main__":
-    hn = [1,2,3,4,5]
 
-    for i in range(0, len(hn)):
-        print(str(goertzel(hn, i)))
+
+    f_s = 4000
+    xn = [1,2,3,4,5]
+    xn = xn + [0] * (int(f_s/2) - len(xn))
+
+    plot_dft(xn, len(xn), f_s, "output")
+
+    for f_m in range(0, f_s, 1000):
+        bpg = BPGoertzel(f_m, f_s)
+        print(f"f_m : {f_m}, mag: {bpg.get_mag(xn)}")
+    
+
+    
+
 
