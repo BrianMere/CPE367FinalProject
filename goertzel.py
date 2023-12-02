90e3c4af9b95b151f5ac5b048e8bd7c78369617f
tommychobo <69866834+tommychobo@users.noreply.github.com>
2023-12-01 20:52:23 -0800

    Doing well, output still nonsensical tho

--- goertzel.py
+++ goertzel.py
@@ -17,19 +17,25 @@ class BPGoertzel:
     information. 
     """
 
-    def __init__(self, f_m : float, f_s : float, resolution : float = 0.01):
-        self.f_m = f_m
-        self.f_s = f_s
-        self.N = int(1/resolution)
-        self.k = round((self.f_m / self.f_s) * self.N)
-        self.sn = my_fifo(self.N)
-        self.wo = 2*math.pi*self.k / self.N
-        self.deq = DifferenceEquation([-2*math.cos(self.wo), 1], [], 1.0)
-        self.fifo_y = my_fifo(self.N)
+    def __init__(self, N: int, k: int):
+        self.N = N
+        self.k = k
+        #self.k = round((self.f_m / self.f_s) * self.N)
+        #self.sn = my_fifo(self.N)
+        self.s_n = 0
+        self.s_n1 = 0
+        self.s_n2 = 0
+        self.wo = 2*math.pi*self.k / self.N # this guy was defined on k and N!
+        #self.deq = DifferenceEquation([-2*math.cos(self.wo), 1], [], 1.0)
+        #self.fifo_y = my_fifo(self.N)
 
         self.flush()
 
     def flush(self):
+        self.s_n = 0
+        self.s_n1 = 0
+        self.s_n2 = 0
+        """
         while not self.sn.is_empty():
             self.sn.dequeue()
         while not self.fifo_y.is_empty():
@@ -37,6 +43,7 @@ class BPGoertzel:
         for _ in range(self.N):
             self.sn.enqueue(0)
             self.fifo_y.enqueue(0)
+        """
     
     def get_mag(self, x_in : float) -> float:
         """Get the magnitude of our class's f_m value. 
@@ -62,50 +69,50 @@ class BPGoertzel:
 
         # Constants 
         j = complex(0.0, 1.0)
-
-        
-        s_n: float = x_in + 2*math.cos(self.wo)*self.sn.get(self.N-1) - self.sn.get(self.N-2)
+        self.s_n2 = self.s_n1
+        self.s_n1 = self.s_n
+        self.s_n: float = x_in + 2*math.cos(self.wo)*self.s_n1 - self.s_n2
         # i_n: float = self.deq.current_yn(x_in)
         # assert s_n == i_n
-        self.sn.dequeue()
-        self.sn.enqueue(s_n)
+        #self.sn.dequeue()
+        #self.sn.enqueue(s_n)
         
-        self.fifo_y.dequeue()
+        #self.fifo_y.dequeue()
         # difference equation for going from s[n] to y[n]
-        y_n : complex = complex(complex(self.sn.get(self.N - 1)) - cmath.exp(-1 * j * self.wo)*self.sn.get(self.N - 2))
-        self.fifo_y.enqueue(y_n)
+        y_n : complex = complex(self.s_n) - cmath.exp(-1 * j * self.wo)*complex(self.s_n1)
+        #self.fifo_y.enqueue(y_n)
         
         return y_n
 
 
 if __name__ == "__main__":
 
-
+    gN = 200
     f_s = 4000
-    xn = [math.cos(2*math.pi*i*0.03) + math.cos(2*math.pi*i * 0.1) for i in range(0, 500)]
+    xn = [math.cos(2*math.pi*i*0.03) + math.cos(2*math.pi*i * 0.1) for i in range(0, gN)]
 
     dft.plot_dft(xn, 32, f_s, "output")
 
     fig, ax = plt.subplots()
 
-    bpg = BPGoertzel(100, f_s, 0.2)
-    bpg2 = BPGoertzel(400, f_s, 0.2)
-    bpg3 = BPGoertzel(600, f_s, 0.2)
+    bpg = BPGoertzel(gN, 2) # 10 Hz, not present
+    bpg2 = BPGoertzel(gN, 20) # 
+    bpg3 = BPGoertzel(gN, 6)
 
     l1 = []
     l2 = []
     l3 = []
     ln = []
 
-    for i, x in enumerate(xn):
-        l1.append(bpg.get_mag(x))
-        l2.append(bpg2.get_mag(x))
-        l3.append(bpg3.get_mag(x))
+    for i in range(0, gN):
+        l1.append(bpg.get_mag(xn[i]))
+        l2.append(bpg2.get_mag(xn[i]))
+        l3.append(bpg3.get_mag(xn[i]))
         ln.append(i)
 
     ax.plot(np.array(ln), np.array(l1))
-    # ax.plot(np.array(ln), np.array(l2))
-    # ax.plot(np.array(ln), np.array(l3))
+    ax.plot(np.array(ln), np.array(l2))
+    ax.plot(np.array(ln), np.array(l3))
     ax.plot(np.array(ln), np.array(xn))
     ax.grid()
 
